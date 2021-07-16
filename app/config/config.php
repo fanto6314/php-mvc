98d141325db6a8cd4db3bbb6b182b5845ab5c32f
fanto6314 <antonio.foresta04@gmail.com>
2021-07-16 17:47:39 +0200

    [+] Added config.php

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_NAME', '');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+    define('URLROOT', 'localhost/mvcframework');
+
+    define('SITENAME', 'fanto6314/php-mvc');
+?>
