cad59df2e723b54ae3aeccad97a304a74d6e5dd8
fanto6314 <antonio.foresta04@gmail.com>
2021-07-16 18:28:49 +0200

    [~] Small Fix

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -2,7 +2,7 @@
     //Load Model and View
     class Controller{
         public function model($model){
-            require_once '../app/models' . $model . '.php';
+            require_once '../app/models/' . $model . '.php';
             return new $model();
         }
         public function view($view, $data = []){
