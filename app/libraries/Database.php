c26ab401035c12a73bdf219b2f1f5f4f26f97446
fanto6314 <antonio.foresta04@gmail.com>
2021-07-16 18:53:04 +0200

    [+] Added classes to Database.php

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -27,8 +27,39 @@
             $this->statement = $this->dbHandler->prepare($sql);
         }
 
-        public function bind ($parameter, $value, $type = null){
-            
+        public function bind($parameter, $value, $type = null) {
+            switch (is_null($type)) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+            $this->statement->bindValue($parameter, $value, $type);
+        }
+        
+        public function execute() {
+            return $this->statement->execute();
+        }
+
+        public function resultSet() {
+            $this->execute();
+            return $this->statement->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        public function single() {
+            $this->execute();
+            return $this->statement->fetch(PDO::FETCH_OBJ);
+        }
+
+        public function rowCount() {
+            return $this->statement->rowCount();
         }
     }
 ?>
