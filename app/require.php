257331e1e4368b5a75a3beb738ada81578fa9d4b
fanto6314 <antonio.foresta04@gmail.com>
2021-07-16 17:47:22 +0200

    [+] Added Config to require.php

--- app/require.php
+++ app/require.php
@@ -4,6 +4,9 @@
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
 
+    // Require config
+    require_once 'config/config.php';
+
     // Instantiating the Core Class
     $init = new Core;
 
